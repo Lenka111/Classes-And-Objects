d1ec05bf9c4672b658cbb1cdcc82bbdc3bbf6cc3
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-10 11:01:15 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
+/*Main class test code*/
 public class Main {
+    
     public static void main(String[] args) {
-
-
-  SimpleCalculator calculator = new SimpleCalculator();
-
-   calculator.setFirstNumber(5.0);
-   calculator.setSecondNumber(4);
+        
+        // create object calculator
+        SimpleCalculator calculator = new SimpleCalculator(); 
+        
+        // set the value of the first number
+        calculator.setFirstNumber(5.0);
+        
+        //set the value of the second number
+        calculator.setSecondNumber(4);
+        
+        //print adition of the values set above
         System.out.println("add = " + calculator.getAdditionResult());
+        
+        //print subtraction of the values set above
         System.out.println("subtract= " + calculator.getSubtractionResult());
-
+        
+        // set the value of the first number
         calculator.setFirstNumber(5.25);
+        
+        //set the value of the second number
         calculator.setSecondNumber(0);
+        
+        // print multiplication and division result 
         System.out.println("multiply = " + calculator.getMultiplicationResult());
         System.out.println("divide = " + calculator.getDivisionResult());
 }
