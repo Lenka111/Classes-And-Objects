c987b3b31f4a1cd901e5f15d2ea9ea7354b82f03
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-10 10:57:01 -0600

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,31 +1,40 @@
 public class SimpleCalculator {
-    // write your code here
-    private double firstNumber;
+    
+   // instance variables(fields)
+    private double firstNumber; 
     private double secondNumber;
 
+    //get the value of firstNumber field
     public double getFirstNumber(){
         return this.firstNumber;
-    }//1
+    }
+    //get the value of secondNumber field
     public double getSecondNumber(){
         return this.secondNumber;
-    }//2
+    }
+    // set the value of firstNumber field
     public void setFirstNumber(double firstNumber){
         this.firstNumber = firstNumber;
-    }//3
+    }
+    //set the value of secondNumber field
     public void setSecondNumber(double secondNumber){
         this.secondNumber = secondNumber;
-    }//4
-
+    }
+    // add the field values of firstNumber and secondNumber
     public double getAdditionResult(){
         return firstNumber + secondNumber;
-    }//5
+    }
+    //subtract the field values of firstNumber and secondNumber
     public double getSubtractionResult(){
         return firstNumber - secondNumber;
-    }//6
+    }
+    //multiply the field values of firstNumber and secondNumber
     public double getMultiplicationResult(){
         return firstNumber * secondNumber;
     }
+    //divide the field values of firstNumber and secondNumber
     public double getDivisionResult(){
+        //if divison by zero return 0
         if (secondNumber==0)
             return 0;
         return firstNumber / secondNumber;
